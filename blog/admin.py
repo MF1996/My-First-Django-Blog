0512f02aee93e1c55d84a194114ba12e700d3533
MF1996 <makam-elchahid@outlook.fr>
2018-05-09 21:34:59 +0000

    Update admin.py
    Permet affichage mieux que de  de faire __str()__

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Post
 
+class Post_admin(admin.Model):
+      list_display = (# nom colone BDD Poste) 
+      
+
+
+
 admin.site.register(Post)
 
 # Register your models here.
